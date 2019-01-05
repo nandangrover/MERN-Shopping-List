225ce1b3d0d4ea307eeb061bb47e90c2d34ebafc
ngrover <nandang@mitrmedia.com>
2019-01-05 17:16:36 +0530

    Server side commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import AppNavBar from './Components/AppNavBar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppNavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://nandan:nandan123@ds041144.mlab.com:41144/shopping-list'
+}
