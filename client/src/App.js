9c5d56e3adc9cf5edd73adc1b991003937cdf456
ngrover <nandang@mitrmedia.com>
2019-01-05 19:08:05 +0530

    Shopping List

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import AppNavBar from './Components/AppNavBar'
+import AppNavBar from './Components/AppNavBar';
+import ShoppingList from './Components/ShoppingList';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavBar />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
