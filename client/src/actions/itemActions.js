d68210cb756c685b5d8b435871c89ce6267126b6
ngrover <nandang@mitrmedia.com>
2019-01-07 19:53:30 +0530

    itemAction.js

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -14,20 +14,38 @@ export const getItems = () => dispatch => {
 }
 
 export const deleteItem = id => dispatch => {
+  dispatch(setItemsLoading());
   axios.delete(`/api/items/${id}`).then(res =>
     dispatch({
       type: DELETE_ITEM,
       payload: id
     }))
+  axios
+    .get('/api/items')
+    .then(res =>
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data
+      })
+    )
 }
 
 export const addItem = item => dispatch => {
+  dispatch(setItemsLoading());
   axios
     .post('/api/items', item)
     .then(res => dispatch({
       type: ADD_ITEM,
       payload: res.data
     }))
+  axios
+    .get('/api/items')
+    .then(res =>
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data
+      })
+    )
 }
 
 export const setItemsLoading = () => {
