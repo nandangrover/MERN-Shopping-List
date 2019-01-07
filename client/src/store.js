e87556f868bea245987a38e33756a350a9d221df
ngrover <nandang@mitrmedia.com>
2019-01-07 19:10:30 +0530

    Heroku update

--- client/src/store.js
+++ client/src/store.js
@@ -6,9 +6,22 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
+const composeEnhancers =
+  typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
   applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+  // other store enhancers if any
+);
+
+const store = createStore(rootReducer, initialState, enhancer);
+// const store = createStore(rootReducer, initialState, compose(
+//   applyMiddleware(...middleware)
+//   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// ))
 
 export default store;
